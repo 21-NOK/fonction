152b23e2bcbad6ff82d16f8a59c50c5550fcaaeb
21-NOK <nolan.kohli@ceff.ch>
2025-01-21 10:59:40 +0100

    Mise à jour pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,6 @@
 /**
  * Well known colors for a NeoPixel strip
  */
-namespace ws2812b {
-    //% shim=sendBufferAsm
-    export function sendBuffer(buf: Buffer, pin: DigitalPin) {
-    }
-    //% shim=setBufferMode
-    export function setBufferMode(pin: DigitalPin, mode: number) {
-    }
-    export const BUFFER_MODE_RGB = 1
-    export const BUFFER_MODE_RGBW = 2
-    export const BUFFER_MODE_RGB_RGB = 3
-    export const BUFFER_MODE_AP102 = 4
-}
 enum NeoPixelColors {
     //% block=red
     Red = 0xFF0000,
